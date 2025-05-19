4ee01bab2791b70932d2ced127571839f65059af
Karine Chen <kari.c.qing@gmail.com>
2025-05-18 22:40:06 -0700

    Display flights info

--- src/components/FlightCard.jsx
+++ src/components/FlightCard.jsx
@@ -1,9 +1,10 @@
 export default function FlightCard({ flight }) {
-return (
-<div style={{ border: '1px solid #ccc', padding: '1rem', marginBottom: '1rem' }}>
-<p><strong>{flight.airline}</strong></p>
-<p>From: {flight.from} → To: {flight.to}</p>
-<p>Price: ${flight.price}</p>
-</div>
-);
+    return (
+        <div style={{ border: '1px solid #ccc', padding: '1rem', marginBottom: '1rem' }}>
+            <p><strong>{flight.airline}</strong></p>
+            <p>From: {flight.from} → To: {flight.to}</p>
+            <p>Price: ${flight.price}</p>
+        </div>
+    );
+   <p></p> 
 }
