bc13d27ad6cf81a2b68d8acf9313d2d0aa763645
Karine Chen <kari.c.qing@gmail.com>
2025-05-19 21:45:44 -0700

    Show list of flights

--- src/components/FlightList.jsx
+++ src/components/FlightList.jsx
@@ -1,10 +1,11 @@
 import FlightCard from './FlightCard';
+
 export default function FlightList({ flights }) {
-return (
-<div>
-{flights.map((flight) => (
-<FlightCard key={flight.id} flight={flight} />
-))}
-</div>
-);
+    return (
+        <div>
+            {flights.map((flight) => (
+            <FlightCard key={flight.id} flight={flight} />
+            ))}
+        </div>
+    );
 }
